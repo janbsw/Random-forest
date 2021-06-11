7a0772d2b8ab70041811276413a4ee1b5f1ba050
Jan Beisswenger <jan@beisswengers.de>
2021-06-11 10:47:46 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+data=pd.read_csv('K:\Code\Random-forest-regression\data\CurrentdetailData.csv')
+
+data=data.apply(preprocessing.LabelEncoder().fit_transform)
+
+features=data.drop('Target',axis=1)
+target=data.iloc[:,5:6].values.reshape(-1,1)
+
+print(target.shape[0])
+print(features.shape[0])
+features_train, features_test, target_train, target_test = train_test_split(features,target,test_size=0.3,random_state=50)
+clf=RandomForestClassifier(n_estimators=200)
+clf.fit(features_train, target_train.ravel())
+
+target_pred=clf.predict(features_test)
+
+print("Accuracy:",metrics.accuracy_score(target_test, target_pred))
---
+++ src/try2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing as le
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+
+def load_dataset():
+    return pd.read_csv('K:\Code\Random-forest-regression\data\CurrentdetailData.csv')
+
+def eveluate_preditctions(target_test,target_pred):
+    return metrics.accuracy_score(target_test, target_pred)
+
+def preprocessing(data):
+    return data.apply(le.LabelEncoder().fit_transform)
+
+def predict(features_train, target_train,features_test):
+    clf=RandomForestClassifier(n_estimators=200)
+    clf.fit(features_train, target_train.ravel())
+    target_pred=clf.predict(features_test)
+    return target_pred
+
+def create_datasets(data,target_label,target):
+    return create_features(data,target_label), create_targets(data,target)
+
+def create_features(data, target_label):
+    return data.drop(target_label,axis=1)
+
+def create_targets(data,target):
+    return data.iloc[:,target-1:target].values.reshape(-1,1)
+
+def create_test_train(feature,target):
+    return train_test_split(feature,target,test_size=0.3,random_state=50)
+
+def main():
+    df=load_dataset()
+    df=preprocessing(df)
+    feature, target = create_datasets(df,"Target",6)
+    features_train, features_test, target_train, target_test = create_test_train(feature,target)
+    target_pred=predict(features_train,target_train,features_test)
+    print("Accuracy",eveluate_preditctions(target_test,target_pred))
+
+
+if __name__ == '__main__':
+    main()

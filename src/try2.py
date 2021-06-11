6b690be536da3b91fe6dd18355aaa886ba600f1a
Jan Beisswenger <jan@beisswengers.de>
2021-06-11 15:03:36 +0200

    Minor changes

--- src/try2.py
+++ src/try2.py
@@ -4,7 +4,11 @@ from sklearn import preprocessing as le
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from sklearn.tree import export_graphviz
-import os
+from subprocess import call
+from sklearn import tree
+from matplotlib import pyplot as plt
+from sklearn.metrics import plot_roc_curve
+
 
 def load_dataset():
     return pd.read_csv('../data/CurrentdetailData.csv')
@@ -38,14 +42,21 @@ def create_test_train(feature,target):
 
 def visualization(clf,feature):
     clff=clf.estimators_[0]
-    export_graphviz(clff,
-                feature_names=feature.columns,
-                filled=True,
-                rounded=True)
-    os.system('dot -Tpng tree.dot -o tree.png')
+    dotfile = open("../tree.dot", 'w')
+    tree.export_graphviz(clff, out_file = dotfile, feature_names = feature.columns)
+    dotfile.close()
 
+def feature_importance(clf,feature):
+    importance=clf.feature_importances_
+    sorted_importance=importance.argsort()
+    plt.barh(feature.columns[sorted_importance],importance[sorted_importance])
+    plt.xlabel("Random Forest Feature Importance")
+    plt.savefig('../graphs/feature_importance.png', bbox_inches="tight")
 
-    
+def roc_curve(clf,feature_test,target_test):
+    plot_roc_curve(clf,feature_test,target_test)
+    plt.savefig('../graphs/roc_curve.png', bbox_inches="tight")
+
 
 def main():
     df=load_dataset()
@@ -54,9 +65,11 @@ def main():
     features_train, features_test, target_train, target_test = create_test_train(feature,target)
     clf=train(features_train,target_train)
     target_pred=preditct(clf,features_test)
-    visualization(clf,feature)
+    #visualization(clf,feature)
+    feature_importance(clf,feature)
+    roc_curve(clf, features_test,target_test)
+    
     print("Accuracy",eveluate_preditctions(target_test,target_pred))
-
 
 if __name__ == '__main__':
     main()
